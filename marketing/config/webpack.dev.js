86b7c3bf3c7d3ffe8fc73c56a0e6d6a08322bad6
Richard Chan <tranchidung0318@gmail.com>
2022-07-17 16:48:12 +0700

    remote

--- marketing/config/webpack.dev.js
+++ marketing/config/webpack.dev.js
@@ -1,20 +1,26 @@
-const { merge } = require('webpack-merge')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const commonConfig = require('./webpack.common')
+const { merge } = require("webpack-merge");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const commonConfig = require("./webpack.common");
+const ModuleFederation = require("webpack/lib/container/ModuleFederationPlugin");
 
 const devConfig = {
-  mode: 'development',
+  mode: "development",
   devServer: {
     port: 8081,
     historyApiFallback: {
-      index: 'index.html'
-    }
+      index: "index.html",
+    },
   },
   plugins: [
-    new HtmlWebpackPlugin({ template: './public/index.html' })
-  ]
-}
+    new ModuleFederation({
+      name: "marketing",
+      filename: "remoteEntry.js",
+      expose: {
+        "./MarketingIndex": "./src/index.js",
+      },
+    }),
+    new HtmlWebpackPlugin({ template: "./public/index.html" }),
+  ],
+};
 
-
-
-module.exports = merge(commonConfig, devConfig)
+module.exports = merge(commonConfig, devConfig);
--- marketing/src/bootstrap.js
+++ marketing/src/bootstrap.js
@@ -1,26 +1,22 @@
-import React from 'react'
-import  ReactDOM  from 'react-dom'
-
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
 //Mount function to start up the app
 const mount = (el) => {
-    ReactDOM.render(
-        <h1>Hi This is Marketing Page</h1>,
-        el
-    )
-}
+  ReactDOM.render(<App />, el);
+};
 
 //if we are in development and in isolation
 //call mount immediately
 if (process.env.NODE_ENV === "development") {
-    const devRoot = document.querySelector("#_marketing-dev-root");
-    //Asumming our container element does not have an element with id dev-products
-    if (devRoot) {
-      mount(devRoot);
-    }
+  const devRoot = document.querySelector("#_marketing-dev-root");
+  //Asumming our container element does not have an element with id dev-products
+  if (devRoot) {
+    mount(devRoot);
   }
+}
 
-
-  //we are running through container
-  //and we should export the moun function
-  export {mount}
+//we are running through container
+//and we should export the moun function
+export { mount };
