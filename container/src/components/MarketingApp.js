40bc2f8163ad7847f0ece43167b903913a619e34
Richard Chan <tranchidung0318@gmail.com>
2022-09-04 22:13:52 +0700

    comment unused components

--- container/src/components/MarketingApp.js
+++ container/src/components/MarketingApp.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useRef } from "react";
 import { mount } from "../../../marketing/src/bootstrap";
 
+//Not using this components use this for review knowledge
 export default () => {
   const ref = useRef(null);
   useEffect(() => {
